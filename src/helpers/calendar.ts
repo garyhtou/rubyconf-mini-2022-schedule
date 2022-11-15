430bdc0107792afdc30350fa89da929c7bb7f504
Gary Tou <gary@garytou.com>
2022-11-15 00:53:11 -0500

    Create calendar events from JSON data

--- src/helpers/calendar.ts
+++ src/helpers/calendar.ts
@@ -1,12 +1,11 @@
-import { getEvents, ET_TZ } from './events';
+import { getEvents, ET_TZ, event } from './events';
 import ical, { ICalEvent, ICalEventData } from 'ical-generator';
-import { Moment } from 'moment-timezone';
 
 async function getCalendar() {
 	const calendar = ical({
-		name: 'RailsConf 2022',
+		name: 'RubyConf Mini 2022',
 		description:
-			'Schedule for RailsConf 2022 in Portland, OR. May 17th - 19th.',
+			'Schedule for RubyConf Mini 2022 in Providence, IR. Nov 15th - 17th.',
 		timezone: ET_TZ,
 	});
 
@@ -14,49 +13,23 @@ async function getCalendar() {
 
 	// add events to calendar
 	events.forEach((e) => {
-		for (let session of e.events) {
-			// console.log({ title: session.title, start: e.start.format() });
-			calendar.createEvent(
-				format(
-					e.start,
-					e.end,
-					session.title,
-					session.labels,
-					session.author,
-					session.location,
-					session.url,
-					session.break
-				)
-			);
-		}
+		calendar.createEvent(format(e));
 	});
 
 	return calendar;
 
-	function format(
-		start: Moment,
-		end: Moment,
-		title: string,
-		labels: string[],
-		author: string,
-		location: string,
-		url: string,
-		isBreak: boolean
-	): ICalEvent | ICalEventData {
-		const t = title + labels.reduce((acc, label) => acc + ` [${label}]`, '');
-
-		const desLabels = labels.length ? 'Track: ' + labels.join(', ') + '\n' : '';
-		const desAuthors = author ? author + '\n' : ''; // sometimes the `author` html element also contains an event description
-		const desUrl = url ? '\n' + url : ''; // not all calendar support the URL field, so we add it to the description
-		const des = desLabels + desAuthors + desUrl;
+	function format(e: event): ICalEvent | ICalEventData {
+		const authors = e.authors ? 'Speaker: ' + e.authors + '\n\n' : '';
+		const url = e.url ? '\n' + e.url : ''; // not all calendar support the URL field, so we add it to the description
+		const des = authors + (e.description || '') + url;
 
 		return {
-			start,
-			end,
-			summary: t,
+			start: e.start,
+			end: e.end,
+			summary: e.title,
 			description: des,
-			location,
-			url,
+			location: e.location,
+			url: e.url,
 			timezone: ET_TZ,
 		};
 	}
